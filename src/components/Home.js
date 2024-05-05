3e6ae1ce9aa91a7e4116b3adefa3d288fe3c5bf7
kiran76 <ksknew76@gmail.com>
2024-05-05 10:49:23 +0530

    Integrated material ui and created listings component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+/**
+ * This is the component file that holds the filter bar and the Job listings (Infinite scrolling )
+ */
+
+import React from "react";
+
+const Home = () =>{
+    return(
+        <div className="home-container">
+            <h1>I'm Home</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/Listings.js
@@ -0,0 +1,16 @@
+/**
+ * This child component has the listings (being fetched from rest api) and infintely scrollable
+ */
+
+import { Grid } from "@mui/material";
+import React from "react";
+
+const Listings = () =>{
+    return(
+        <Grid container spacing={4}>
+
+        </Grid>
+    )
+}
+
+export default Listings;
