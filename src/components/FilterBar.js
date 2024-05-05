fb28c64966a76af1fe22681d00a1e6baf3bfb3b6
kiran76 <ksknew76@gmail.com>
2024-05-05 11:26:33 +0530

    Added Roles DropDown

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,13 +2,34 @@
  * This is the filter bar where we can filter the listings based on Role, etc
  */
 import '../App.css';
-import { Typography } from "@mui/material";
+import { TextField, Typography } from "@mui/material";
 import React from "react";
 
 const Filter = () =>{
     return(
         <div className="filters">
-            <Typography variant="h1">This is where Filters go</Typography>
+            <TextField
+             select
+             SelectProps={{native: true}}
+             className=''
+             value='Role'
+            >
+                <option key="default" value="">
+                    Roles
+                </option>
+
+                <option key="default" value="SWE">
+                    SWE
+                </option>
+
+                <option key="default" value="Product Manager">
+                    Product Manager
+                </option>
+
+                <option key="default" value="Intern">
+                    Intern
+                </option>
+            </TextField>
         </div>
     )
 }
